a293be3716925585c078fa20661030132f5475cf
Svitlana Sliusar <svitlana_sliusar@epam.com>
2019-06-30 21:38:42 +0300

    Add project files.

---
+++ StatisticsAnalysis/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace StatisticsAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.methodName = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(51, 25);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(95, 22);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load 1 ";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(292, 24);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(95, 22);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save to EXCEL";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "(.dcm) dataset";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(51, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Pixels data";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(243, 216);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 45);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(95, 10);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(292, 45);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(95, 10);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // methodName
+            // 
+            this.methodName.FormattingEnabled = true;
+            this.methodName.Items.AddRange(new object[] {
+            "Brute Force",
+            "Gold Standart",
+            "Otsu",
+            "Niblack"});
+            this.methodName.Location = new System.Drawing.Point(393, 25);
+            this.methodName.Name = "methodName";
+            this.methodName.Size = new System.Drawing.Size(121, 21);
+            this.methodName.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "New Dataset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 368);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.methodName);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ComboBox methodName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StatisticsAnalysis/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EvilDICOM.Core.Helpers;
+using EvilDICOM.Core.Interfaces;
+using EvilDICOM.Core.IO.Writing;
+using EvilDICOM.Core.Element;
+using EvilDICOM.Core;
+using ClosedXML.Excel;
+
+namespace StatisticsAnalysis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct DicomData
+        {
+            public dynamic[] PixelDataOrig;
+            public int sizeRow;
+            public int sizeCol;
+            public string fileName;
+            public double sliceLoc;
+            public double instNum;
+            public string fullFilePath;
+        }
+
+        DicomData[] firstDataSet;
+        DicomData[] secondDataSet;
+        bool firstComplete = false;
+        bool secondComplete = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if (browserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string dir = browserDialog.SelectedPath;
+                string[] fullfilesPath = Directory.GetFiles(dir, "*.dcm", SearchOption.AllDirectories);
+                progressBar1.Maximum = fullfilesPath.Length;
+                firstDataSet = LoadFiles(fullfilesPath);
+                firstComplete = true;
+            }
+        }
+
+        XLWorkbook workBook = new XLWorkbook();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(firstDataSet, workBook);
+        }
+
+        private DicomData[] LoadFiles(string[] fullfilesPath)
+        {
+            DICOMObject dicom;
+            DicomData[] DicomFile = new DicomData[fullfilesPath.Length];
+            for (int curImg = 0; curImg < fullfilesPath.Length; curImg++)
+            {
+                if (!firstComplete)
+                    progressBar1.Value = curImg;
+                else
+                    progressBar2.Value = curImg;
+                string file = fullfilesPath[curImg];
+                dicom = DICOMObject.Read(file);
+
+                DicomFile[curImg].sizeRow = Convert.ToInt32(dicom.FindFirst("00280010").DData);
+                DicomFile[curImg].sizeCol = Convert.ToInt32(dicom.FindFirst("00280011").DData);
+                DicomFile[curImg].fileName = Path.GetFileName(fullfilesPath[curImg]);
+                DicomFile[curImg].fullFilePath = fullfilesPath[curImg];
+
+                var pixelData = dicom.FindFirst("7FE00010").DData_;
+                DicomFile[curImg].PixelDataOrig = new dynamic[DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol];
+                var typeData = Convert.ToInt16(dicom.FindFirst("00280100").DData);
+                switch (typeData)
+                {
+                    case 8:
+                        if (pixelData.Count != DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol)
+                        {
+                            dynamic[] temp = new dynamic[pixelData.Count];
+                            pixelData.CopyTo(temp, 0);
+                            dynamic[] temp2 = new dynamic[DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol];
+                            temp2 = temp.Skip(1).Take(DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol).ToArray();
+                            for (int i = 0; i < temp2.Length; i++)
+                                temp2[i] = temp2[i] ?? 0;
+                            temp2.CopyTo(DicomFile[curImg].PixelDataOrig, 0);
+                        }
+                        else
+                            pixelData.CopyTo(DicomFile[curImg].PixelDataOrig, 0);
+                        break;
+                    case 16:
+                        var PixelDataArray = new ushort[DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol * 2];
+                        pixelData.CopyTo(PixelDataArray, 0);
+                        int pix = 0;
+                        for (int i = 0; i < DicomFile[curImg].sizeRow * DicomFile[curImg].sizeCol * 2; i += 2)
+                            DicomFile[curImg].PixelDataOrig[pix++] = (ushort)(PixelDataArray[i] | PixelDataArray[i + 1] << 8);
+                        break;
+                }
+                
+                var rescaleSlope = (float)Convert.ToDouble(dicom.FindFirst("00281053").DData);
+                var rescaleIntercept = (float)Convert.ToDouble(dicom.FindFirst("00281052").DData);
+                for (int i=0; i<DicomFile[curImg].PixelDataOrig.Length;i++)
+                {
+                    DicomFile[curImg].PixelDataOrig[i] = DicomFile[curImg].PixelDataOrig[i] * rescaleSlope + rescaleIntercept;
+                }
+            }
+            return DicomFile;
+        }
+
+        private void ExportToExcel(DicomData[] dicomDatas, XLWorkbook workBook)
+        { 
+            var workSheet = workBook.Worksheets.Add(methodName.SelectedItem.ToString());
+            //IXLWorksheet workSheet;
+            //workBook.TryGetWorksheet(methodName.SelectedItem.ToString(), out workSheet);
+            int columnCount = 0;
+            foreach (var data in dicomDatas)
+            {
+                progressBar2.Value = columnCount;
+                workSheet.Cell(1, ++columnCount).Value = Path.GetFileNameWithoutExtension(data.fileName);
+                for (int i = 1; i < data.PixelDataOrig.Length; i++)
+                {
+                    workSheet.Cell(i + 1, columnCount).SetValue<dynamic>(data.PixelDataOrig[i]);
+                }                
+            }
+            workSheet.Columns().AdjustToContents();
+            //workBook.Save();
+            workBook.SaveAs(String.Format("D://{0}//{0}.xlsx", 
+                dicomDatas[0].fileName.Substring(0, 12)));
+            //int i = 0; 
+
+            //foreach(var dicomData in dicomDatas)
+            //{
+            //    i = 0;
+            //    table.Columns.Add(dicomData.fileName, typeof(float));
+            //    foreach(var pixelData in dicomData.PixelDataOrig)
+            //    {
+            //        try
+            //        {
+            //            table.Rows[i][dicomData.fileName] = pixelData;
+            //            i++;
+            //        }
+            //        catch(System.IndexOutOfRangeException)
+            //        {
+            //            table.Rows.Add(pixelData);
+            //        }
+            //    }
+            //}
+            //workBook.Worksheets.Add(table, "BRUTEFORCE");
+
+
+            //var workbook = new XLWorkbook();
+            //var worksheet = workbook.Worksheets.Add("Sample Sheet");
+            //worksheet.Cell("A1").Value = "Hello World!";
+            //workbook.SaveAs("D://A//hello.xlsx");
+            //System.Data.DataTable table = new System.Data.DataTable();
+            //table.Columns.Add("First Dataset", typeof(ushort));
+            //table.Columns.Add("Second Dataset", typeof(ushort));
+            //table.Columns.Add("Pearson");
+            //table.Columns.Add("Slope");
+            //table.Columns.Add("Intercept");
+            //Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            //excel.Workbooks.Add();
+            //excel.Visible = false;
+            //excel.Range["A1"].Value = "Pearson";
+            //excel.Range["A2"].Select();
+            //for (int i = 0, j = 0; i < firstDataSet[0].PixelDataOrig.Length && j < secondDataSet[0].PixelDataOrig.Length; i++, j++)
+            //{
+            //    excel.ActiveCell.Value = firstDataSet[0].PixelDataOrig[i];
+            //    excel.ActiveCell.Offset[0, 1].Value = secondDataSet[0].PixelDataOrig[j];
+            //    excel.ActiveCell.Offset[1, 0].Select();
+            //}
+
+            //WorksheetFunction worksheetFunction = excel.WorksheetFunction;
+            //List<double> pearsons = new List<double>();
+            //for (int i = 0, j = 0; i < firstDataSet.Length && j < secondDataSet.Length; i++, j++)
+            //{
+            //    try
+            //    {
+            //        pearsons.Add(worksheetFunction.Pearson(firstDataSet[i].PixelDataOrig, secondDataSet[j].PixelDataOrig));
+            //    }
+            //    catch(Exception e)
+            //    {
+
+            //    }
+            //        //excel.ActiveCell.Offset[1, 0].Select();
+            //}
+            //excel.Columns[1].AutoFit();
+            //excel.Visible = true;
+            //return table;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            workBook = new XLWorkbook();
+        }
+    }
+}
